332702c9d99ce1fe17c1088e1fd7ab6f6165625f
pedrolucassantoscunha <pedrolucassantoscunha2@gmail.com>
2022-07-05 16:57:41 -0300

    Sacar

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -1,15 +1,30 @@
-public class ContaCorrente
+public class ContaCorrente // Classe
 {
    public string Titular;
    public int Numero;
    public int Agencia;
    public double Saldo;
 
-public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo)
+
+public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo)  // Construtor
 {
    this.Titular = Titular;
    this.Agencia = Agencia;
    this.Numero = Numero;
    this.Saldo = Saldo;
 }
+
+//Construtuor nao tem retorno
+
+public bool Sacar (double valorSaque) // Metodo
+{
+   if (this.Saldo >= valorSaque)
+   {
+      this.Saldo = this.Saldo - valorSaque;
+      return true;
+   }
+   return false;
+}
+
+
 }
--- Program.cs
+++ Program.cs
@@ -7,19 +7,33 @@ namespace ProjetoC
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta_toddy = new ContaCorrente ("Toddy", 12345, 789, 10);
-            ContaCorrente conta_pedro = new ContaCorrente ("Pedro", 653633, 234,1);
-            ContaCorrente conta_lucas = new ContaCorrente ("Lucas", 7502, 756, -100);
+            // Objetos
+            ContaCorrente conta_toddy = new ContaCorrente ("Toddy", 12345, 789, 1000);
+            ContaCorrente conta_pedro = new ContaCorrente ("Pedro", 653633, 234,600);
+            ContaCorrente conta_lucas = new ContaCorrente ("Lucas", 7502, 756, 100);
 
 
             Console.WriteLine("A conta é do (a) " + conta_toddy.Titular + ", a agência é " + conta_toddy.Agencia
-            + " e o número é " + conta_toddy.Numero);
+            + " e o número é " + conta_toddy.Numero+ " Saldo: " + conta_toddy.Saldo);
             
             Console.WriteLine("A conta é do (a) " + conta_pedro.Titular + ", a agência é " + conta_pedro.Agencia
-            + " e o número é " + conta_pedro.Numero);
+            + " e o número é " + conta_pedro.Numero+ " Saldo: " + conta_pedro.Saldo);
             
             Console.WriteLine("A conta é do (a) " + conta_lucas.Titular + ", a agência é " + conta_lucas.Agencia
-            + " e o número é " + conta_lucas.Numero);
+            + " e o número é " + conta_lucas.Numero + " Saldo: " + conta_lucas.Saldo);
+
+            bool sacar_conta = conta_toddy.Sacar(100);
+            bool sacar_conta1 = conta_pedro.Sacar(100);
+            bool sacar_conta2 = conta_lucas.Sacar(100);
+
+            Console.WriteLine("A conta é do (a) " + conta_toddy.Titular + ", a agência é " + conta_toddy.Agencia
+            + " e o número é " + conta_toddy.Numero+ " Saldo: " + conta_toddy.Saldo);
+            
+            Console.WriteLine("A conta é do (a) " + conta_pedro.Titular + ", a agência é " + conta_pedro.Agencia
+            + " e o número é " + conta_pedro.Numero+ " Saldo: " + conta_pedro.Saldo);
+            
+            Console.WriteLine("A conta é do (a) " + conta_lucas.Titular + ", a agência é " + conta_lucas.Agencia
+            + " e o número é " + conta_lucas.Numero + " Saldo: " + conta_lucas.Saldo);
         }
     }
 }
